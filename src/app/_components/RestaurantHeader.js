fa4fdd3f05d5c904a2f0c0081ff0e3e42f0f0537
soumyakant mahakud <150366334+soumyakmahakud@users.noreply.github.com>
2024-04-23 15:36:42 +0530

    localstorage_for_Signup

--- src/app/_components/RestaurantHeader.js
+++ src/app/_components/RestaurantHeader.js
@@ -1,20 +1,41 @@
+"use client"
 import Link from 'next/link'
-const RestaurantHeader = ()=> {
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+const RestaurantHeader = () => {
+    const [details, setDetails] = useState();
+    const router = useRouter();
+    const pathName = usePathname();
+    useEffect(() => {
+        let data = localStorage.getItem("restoUser");
+        if (!data) {
+            router.push("/restaurant")
+        } else if(data && pathName == '/restaurant'){
+            router.push("/restaurant/dashboard")
+        }else {
+            setDetails(JSON.parse(data))
+        }
+    },[])
+    const logout =()=> {
+        localStorage.removeItem("restoUser");
+        router.push("/restaurant")
+    }
     return (
         <div className='header-wrapper'>
             <div className="logo">
-                <img style={{width:100}} src="" alt='logo'/>
+                <img style={{ width: 100 }} src="" alt='logo' />
             </div>
             <ul>
-                <li>
-                    <Link href={"/"}>Home</Link>
-                </li>
-                <li>
-                    <Link href={"/"}>Login/Signup</Link>
-                </li>
-                <li>
-                    <Link href={"/"}>Profile</Link>
-                </li>
+                <Link href={"/"}>Home</Link>
+                {
+                    details && details.name ?
+                        <>
+                            <li><button onClick={logout}>Logout</button></li>
+                            <li><Link href="/">Profile</Link></li>
+                        </>
+                        : <li><Link href="/">Login/Signup</Link></li>
+                }
             </ul>
         </div>
     )
--- src/app/_components/RestaurantSignup.js
+++ src/app/_components/RestaurantSignup.js
@@ -1,6 +1,8 @@
+
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-const RestaurantSignup = ()=> {
+const RestaurantSignup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [c_password, setC_password] = useState('')
@@ -9,16 +11,23 @@ const RestaurantSignup = ()=> {
     const [address, setAddress] = useState('')
     const [contact, setContact] = useState('')
 
-    const handleSignup = async ()=> {
+    const router = useRouter();
+
+    const handleSignup = async () => {
         console.log(email, password, name, city, address, contact);
-        let result = await fetch("http://localhost:3000/api/restaurant", {
-            method:"POST",
-            body:JSON.stringify({email, password, name, city, address, contact})
+        let response = await fetch("http://localhost:3000/api/restaurant", {
+            method: "POST",
+            body: JSON.stringify({ email, password, name, city, address, contact })
         })
-        result = await result.json();
-        console.log(result);
-        if(result.success){
-            alert("Restaurant Registered successfully")
+        response = await response.json();
+        console.log(response);
+        if (response.success) {
+            console.log(response);
+            const { result } = response
+            delete result.password;
+            // localStorage.getItem("restaurantUser", JSON.stringify(result));
+            localStorage.setItem('restoUser', JSON.stringify(result))
+            router.push("/restaurant/dashboard")
         }
     }
     return (
@@ -26,32 +35,32 @@ const RestaurantSignup = ()=> {
             <h3>Signup</h3>
             <div>
                 <div className="input-wrapper">
-                    <input type="email" placeholder="Enter email id" className="input-field" 
-                    value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="email" placeholder="Enter email id" className="input-field"
+                        value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className="input-wrapper">
                     <input type="password" placeholder="Enter password" className="input-field"
-                    value={password} onChange={(e) => setPassword(e.target.value)} />
+                        value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div className="input-wrapper">
                     <input type="password" placeholder="confirm password" className="input-field"
-                    value={c_password} onChange={(e) => setC_password(e.target.value)} />
+                        value={c_password} onChange={(e) => setC_password(e.target.value)} />
                 </div>
                 <div className="input-wrapper">
                     <input type="text" placeholder="restaurant name" className="input-field"
-                    value={name} onChange={(e) => setName(e.target.value)} />
+                        value={name} onChange={(e) => setName(e.target.value)} />
                 </div>
                 <div className="input-wrapper">
                     <input type="text" placeholder="Enter city" className="input-field"
-                    value={city} onChange={(e) => setCity(e.target.value)} />
+                        value={city} onChange={(e) => setCity(e.target.value)} />
                 </div>
                 <div className="input-wrapper">
                     <input type="text" placeholder="Enter full address" className="input-field"
-                    value={address} onChange={(e) => setAddress(e.target.value)} />
+                        value={address} onChange={(e) => setAddress(e.target.value)} />
                 </div>
                 <div className="input-wrapper">
                     <input type="text" placeholder="Enter contact number" className="input-field"
-                    value={contact} onChange={(e) => setContact(e.target.value)} />
+                        value={contact} onChange={(e) => setContact(e.target.value)} />
                 </div>
                 <div className="input-wrapper">
                     <button className="button" onClick={handleSignup}>Signup</button>
