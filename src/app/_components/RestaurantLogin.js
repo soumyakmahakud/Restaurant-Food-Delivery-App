1cdbbbad0f55a168d2c29729f8e1cf28411b16e0
soumyakant mahakud <150366334+soumyakmahakud@users.noreply.github.com>
2024-04-23 16:05:54 +0530

    Login Validation

--- src/app/_components/RestaurantLogin.js
+++ src/app/_components/RestaurantLogin.js
@@ -1,17 +1,37 @@
+import { useState } from "react";
 
 const RestaurantLogin = () => {
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [error, setError] = useState(false)
+
+    const handleLogin = ()=> {
+     if(!email || !password) {
+        setError(true)
+     } else {
+        setError(false)
+     }
+    }
     return (
         <>
             <h3>Login Component</h3>
             <div>
                 <div className="input-wrapper">
-                    <input type="text" placeholder="Enter email id" className="input-field" />
+                    <input type="text" placeholder="Enter email" className="input-field"
+                    value={email} onChange={(e)=> setEmail(e.target.value)} />
+                    {
+                        error && !email && <span className="input_error">Please enter valid email</span>
+                    }
                 </div>
                 <div className="input-wrapper">
-                    <input type="password" placeholder="Enter password" className="input-field" />
+                    <input type="password" placeholder="Enter password" className="input-field" 
+                    value={password} onChange={(e)=> setPassword(e.target.value)} />
+                    {
+                        error && !password && <span className="input_error">Please enter valid password</span>
+                    }
                 </div>
                 <div className="input-wrapper">
-                    <button className="button">Login</button>
+                    <button className="button" onClick={handleLogin}>Login</button>
                 </div>
             </div>
         </>
