c72f15ba165c2d9125c59184744c39246ead4e63
soumyakant mahakud <150366334+soumyakmahakud@users.noreply.github.com>
2024-04-26 12:17:24 +0530

    add food validation

--- src/app/_components/AddFoodItem.js
+++ src/app/_components/AddFoodItem.js
@@ -6,8 +6,16 @@ const AddFoodItem = () => {
   const [path, setPath] = useState("");
   const [description, setDescription] = useState("");
 
+  const [error, setError] = useState(false)
+
   const handleAddFoodItem = async ()=> {
     console.log(name, path, price, description);
+    if (!name || !price || !path || !description) {
+      setError(true);
+      return false
+    } else {
+      setError(false)
+    }
     let resto_id;
     const restaurantData = JSON.parse(localStorage.getItem("restoUser"));
     if (restaurantData) {
@@ -21,7 +29,9 @@ const AddFoodItem = () => {
     });
     response = await response.json();
     if (response.success) {
-      alert("hhhh")
+      alert("Food item added")
+    } else {
+      alert("Food item not added")
     }
   }
 
@@ -30,15 +40,27 @@ const AddFoodItem = () => {
       <h1>Add New Food Item</h1>
       <div className='input-wrapper'>
         <input type="text" placeholder="Enter food name" className="input-field" value={name} onChange={(e) => setName(e.target.value)} />
+        {
+          error && !name && <span className="input_error">please enter valid name</span>
+        }
       </div>
       <div className='input-wrapper'>
         <input type="text" placeholder="Enter price" className="input-field" value={price} onChange={(e) => setPrice(e.target.value)} />
+        {
+          error && !price && <span className="input_error">please enter valid price</span>
+        }
       </div>
       <div className='input-wrapper'>
         <input type="text" placeholder="Enter image path" className="input-field" value={path} onChange={(e) => setPath(e.target.value)} />
+        {
+          error && !path && <span className="input_error">please enter valid path</span>
+        }
       </div>
       <div className='input-wrapper'>
         <input type="text" placeholder="Enter description" className="input-field" value={description} onChange={(e) => setDescription(e.target.value)} />
+        {
+          error && !description && <span className="input_error">please enter valid description</span>
+        }
       </div>
       <div className='input-wrapper'>
         <button className='button' onClick={handleAddFoodItem}>Add Food Item</button>
--- src/app/api/restaurant/foods/route.js
+++ src/app/api/restaurant/foods/route.js
@@ -5,8 +5,12 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
     const payload = await request.json();
+    let success = false
     await mongoose.connect(connectionStr, {useNewUrlParser:true});
     const food = new foodSchma(payload);
     const result = await food.save();
+    if (result) {
+        success = true
+    }
     return NextResponse.json({result, success:true})
 }
