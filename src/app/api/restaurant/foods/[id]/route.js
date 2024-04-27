7a602c35e270f94c35854ef992b3521f4f9f9a88
soumyakant mahakud <150366334+soumyakmahakud@users.noreply.github.com>
2024-04-27 12:41:17 +0530

    food add and delete

--- src/app/api/restaurant/foods/[id]/route.js
+++ src/app/api/restaurant/foods/[id]/route.js
@@ -1,15 +1,27 @@
 import { connectionStr } from "@/app/lib/db";
-import { foodSchma } from "@/app/lib/foodsModel";
+import { foodSchema } from "@/app/lib/foodsModel";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
-export async function GET(request, content) {
+export async function GET(request,content){
     const id = content.params.id
-    let success = false;
-    await mongoose.connect(connectionStr, {useNewUrlParser:true})
-    const result =await foodSchma.find({resto_id:id});
-    if (result) {
+    let success=false;
+    await mongoose.connect(connectionStr,{useNewUrlParser:true});
+    const result = await foodSchema.find({resto_id:id});
+    if(result){
         success=true
     }
-    return NextResponse.json({result, success})
+    return NextResponse.json({result,success})
+
+}
+export async function DELETE(request,content){
+    const id = content.params.id;
+    let success = false;
+    await mongoose.connect(connectionStr,{useNewUrlParser:true});
+    const result = await foodSchema.deleteOne({_id:id})
+    if(result.deletedCount>0){
+        success=true
+    }
+
+    return NextResponse.json({result,success})
 }
---
+++ src/app/api/restaurant/foods/edit/[id]/route.js
@@ -0,0 +1,7 @@
+import { foodSchma } from "@/app/lib/foodsModel";
+
+export async function GET(request, content) {
+    const id = content.params.id;
+    let success = false;
+    await result = await foodSchma.fim
+}
